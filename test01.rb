2b5863e5d3da315f796d4fc4ad30e720d5a51413
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-11-28 03:29:40 +0900

    [Fix] Fixed the answer for Question 01.
    Fixed to output per IP address.

--- test01.rb
+++ test01.rb
@@ -7,55 +7,63 @@ file = File.open("./sample.log", "r").read
 logs = file.split("\n")
 
 # ログをKey-Valueに細分化
-log_details = logs.map.with_index do |log, index|
+log_details = logs.map do |log|
   datetime, ip, ping_response = log.split(",")
   datetime = Time.parse(datetime)
   { datetime: datetime, ip: ip, ping_response: ping_response }
 end
 
+# IPアドレスごとにログを分割
+log_details_per_ip = log_details.group_by{|ld| ld[:ip]}
 
-# タイムアウトしたログとレスポンスが復活したログのインデックス番号を取得
-timeout_indexes_sets = log_details.map.with_index do |log_detail, index|
-  if (log_detail[:ping_response] == "-")
-    next if (log_details[index-1][:ping_response] == "-")
-    start_index = index
-    ok_log_index = log_details[index..-1].find_index{|v| v[:ping_response] != "-"}
+# IPアドレス毎に、タイムアウトになった時間・復旧した時間のインデックスを取得
+timeouts_per_ip = {}
+log_details_per_ip.keys.each{ |ip| timeouts_per_ip[ip] = [] }
+log_details_per_ip.map do |ip, lds|
+  lds.map.with_index do |ld, i|
+    if (ld[:ping_response] == "-")
+      next if (lds[i-1][:ping_response] == "-") # 連続してタイムアウトした場合はタイムアウト時間を1つにまとめるので
+      start_index = i
+      ok_log_index = lds[i..-1].find_index{|v| v[:ping_response] != "-"}
 
-    # 最終ログのステータスがタイムアウトの場合で条件分岐
-    if ok_log_index.nil?
-      end_index = nil
-    else
-      end_index = index + ok_log_index
+      # 最終ログのステータスがタイムアウトの場合で条件分岐
+      if ok_log_index.nil?
+        end_index = nil
+      else
+        end_index = i + ok_log_index
+      end
+      timeouts_per_ip[ip] << { start_index: start_index, end_index: end_index }
     end
-    {start_index: start_index, end_index: end_index}
   end
-end.compact
-
-
-# タイムアウト期間中のサーバーIP、タイムアウト開始時間・復旧時間・タイムアウト期間をKey-Value型に整形
-timeouts = timeout_indexes_sets.map do |set|
-  start_index, end_index = set[:start_index], set[:end_index]
-  ip = log_details[start_index][:ip]
-  timeout_start = log_details[start_index][:datetime]
-  if end_index.nil?
-    timeout_end = nil
-    period = "タイムアウト中。まだ復旧していない。"
-  else
-    timeout_end = log_details[end_index][:datetime]
-    minites, seconds = (timeout_end - timeout_start).divmod(60).map(&:to_i)
-    hour, minites = minites.divmod(60).map(&:to_i)
-    period = "#{hour}時間#{minites}分#{seconds}秒"
-  end
-  {ip: ip, start: timeout_start, end: timeout_end, period: period}
 end
 
 
+# IPアドレス毎に、タイムアウト開始時間・復旧時間・タイムアウト期間をKey-Value型に整形
+results = timeouts_per_ip.map{ |ip, timeouts|
+  timeouts.map do |timeout|
+    start_index, end_index = timeout[:start_index], timeout[:end_index]
+    lds = log_details_per_ip[ip]
+    timeout_start = lds[start_index][:datetime]
+    if end_index.nil?
+      timeout_end = nil
+      period = "タイムアウト中。まだ復旧していない。"
+    else
+      timeout_end = lds[end_index][:datetime]
+      minites, seconds = (timeout_end - timeout_start).divmod(60).map(&:to_i)
+      hour, minites = minites.divmod(60).map(&:to_i)
+      period = "#{hour}時間#{minites}分#{seconds}秒"
+    end
+    { ip: ip, start: timeout_start, end: timeout_end, period: period }
+  end
+}.flatten
+
+
 # 故障状態のサーバアドレスとそのサーバの故障期間を標準出力
-timeouts.each do |timeout|
+results.each do |res|
   mes = <<~EOS
   タイムアウト発生
-  IPアドレス: #{timeout[:ip]}
-  期間: #{timeout[:start]} ~ #{timeout[:end]} ( #{timeout[:period]} )
+  IPアドレス: #{res[:ip]}
+  期間: #{res[:start]} ~ #{res[:end]} ( #{res[:period]} )
   EOS
   puts mes
   puts "="*100
